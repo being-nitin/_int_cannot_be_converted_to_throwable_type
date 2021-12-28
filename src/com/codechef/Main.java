8af499dd7cc2406882c9c83898b971160379e7db
Nitin <nsinghboss02@gmail.com>
2021-12-28 18:38:06 +0530

    Initial commit

---
+++ src/com/codechef/Main.java
@@ -0,0 +1,33 @@
+package com.codechef;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// throw keyword to throw an exception:
+        try
+        {
+            System.out.print("A");
+            throw 99;
+        }
+        catch (int ex)
+        {
+            System.out.print("B");
+        }
+/*
+    JavaHungry.java:7: error: incompatible types: int cannot be converted to Throwable
+            throw 99;
+            ^
+    JavaHungry.java:9: error: unexpected type
+        catch (int ex)
+               ^
+    required: class
+    found:    int
+    2 errors
+ */
+
+
+
+
+
+    }
+}
